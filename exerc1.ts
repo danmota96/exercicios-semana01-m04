df3d989ad735147a443b97494b8ece19b73f23fe
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-05-19 21:06:19 -0300

    Exercícios - Semana 01
    Exercícios 1, 2 e 3 em typescript.

--- exerc1.ts
+++ exerc1.ts
@@ -1,3 +1,9 @@
+/* Estamos começando uma pequena comunidade de usuários. 
+Por motivos de desempenho, decidimos armazenar todos os usuários diretamente no código. Dessa forma, podemos fornecer aos nossos desenvolvedores mais oportunidades de interação com o usuário.
+Todos os problemas relacionados ao Regulamento Geral sobre a Proteção de Dados serão resolvidos algum outro dia. Esta seria a base para nossos futuros experimentos durante esses exercícios.
+Dados os dados, defina a interface "User" e use-a de acordo.
+ */
+
 type User = {
     name: string;
     age: number;
